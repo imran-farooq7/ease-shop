4ca43c2a61d5dbe2bf8feae6d92b0d625dfbdb86
Imran Farooq <imran_farooq7@hotmail.com>
2024-07-31 01:47:05 +0500

    Stripe added with prisma user update

--- auth.ts
+++ auth.ts
@@ -2,9 +2,26 @@ import NextAuth from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
+import { stripe } from "./utils/Constants";
 const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [Google],
+	events: {
+		createUser: async ({ user }) => {
+			const customer = await stripe.customers.create({
+				email: user.email!,
+				name: user.name!,
+			});
+			await prisma.user.update({
+				where: {
+					id: user.id,
+				},
+				data: {
+					stripeId: customer.id,
+				},
+			});
+		},
+	},
 });
